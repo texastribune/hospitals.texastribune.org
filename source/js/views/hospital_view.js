0027f8eef7dc68e0e5eb19f2ea43ee2acbca7fce
malev <marcosvanetta@gmail.com>
2014-05-30 18:13:46 -0500

    Check/uncheck hospitals working

--- source/js/views/hospital_view.js
+++ source/js/views/hospital_view.js
@@ -4,10 +4,23 @@ app.Views.Hospital = Marionette.ItemView.extend({
   template: JST['templates/hospital-cell'],
   tagName: 'tr',
   className: 'hospital',
+
+  ui: {
+    'checkbox': '.hospital-selector'
+  },
+
+  events: {
+    'click @ui.checkbox' : 'hospitalSelected'
+  },
+
   templateHelpers: function(){
     var showDistance = this.model.hasDistance();
     return {
       showDistance: showDistance
     };
+  },
+
+  hospitalSelected: function(){
+    this.trigger('select:hospital');
   }
 });
--- source/js/views/hospitals_view.js
+++ source/js/views/hospitals_view.js
@@ -4,6 +4,7 @@ app.Views.Hospitals = Marionette.CompositeView.extend({
   template: JST['templates/hospitals'],
   itemView: app.Views.Hospital,
   emptyView: app.Views.HospitalsEmpty,
+
   templateHelpers: function(){
     var showDistance = this.collection.at(0).hasDistance();
     return {
@@ -11,7 +12,19 @@ app.Views.Hospitals = Marionette.CompositeView.extend({
     };
   },
 
+  itemEvents: {
+    'select:hospital': 'selectHospital',
+  },
+
   appendHtml: function(collectionView, itemView){
     collectionView.$("tbody").append(itemView.el);
-  }
+  },
+
+  selectHospital: function(){
+    if ($('.hospital-selector:checked').length < 3){
+      $('.hospital-selector').removeAttr('disabled');
+    } else {
+      $('.hospital-selector:not(:checked)').attr('disabled', 'disabled');
+    }
+  },
 });
