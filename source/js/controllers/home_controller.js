67e16a475eacb0443bc19c0ce976be2d60d24e04
malev <marcosvanetta@gmail.com>
2014-06-03 18:28:26 -0500

    Made code compliant with coding styles

--- source/js/controllers/home_controller.js
+++ source/js/controllers/home_controller.js
@@ -1,31 +1,31 @@
-"use strict";
+'use strict';
 
-app.Controllers.HomeController = Marionette.Controller.extend({
-  initialize: function(){
+app.Controllers.HomeController = Marionette.Controller.extend( {
+  initialize: function() {
     this.perPage = 10;
     this.searchView = new app.Views.Search();
     this.hospitals = new app.Collections.Hospitals();
     this.hospitals.fetch();
     this.fetchZipcodes();
 
-    this.listenTo(this.searchView, "call:search", this.search);
-    this.listenTo(this, "after:search", this.afterSearch);
-    this.listenTo(this, "error:search", this.errorSearch);
+    this.listenTo(this.searchView, 'call:search', this.search);
+    this.listenTo(this, 'after:search', this.afterSearch);
+    this.listenTo(this, 'error:search', this.errorSearch);
     this.centerLocation = [];
 
     app.searchRegion.show(this.searchView);
   },
 
-  index: function(){},
+  index: function() {},
 
-  show: function(id){},
+  show: function(id) {},
 
-  errorSearch: function(msg){
+  errorSearch: function(msg) {
     this.hideResults();
     this.searchView.showError(msg);
   },
 
-  afterSearch: function(findings){
+  afterSearch: function(findings) {
     this.hideResults();
     if(findings.length === 0){
       this.searchView.showEmpty();
@@ -34,7 +34,7 @@ app.Controllers.HomeController = Marionette.Controller.extend({
     }
   },
 
-  search: function(cad, page){
+  search: function(cad, page) {
     this.page = typeof page !== 'undefined' ?  page : 1;
     if(cad === 'nearest') {
       this.nearest(this.page);
@@ -45,26 +45,28 @@ app.Controllers.HomeController = Marionette.Controller.extend({
     }
   },
 
-  nearest: function(page){
+  nearest: function(page) {
     var self = this,
         findings;
-    if ("geolocation" in navigator){
+
+    if ('geolocation' in navigator) {
       navigator.geolocation.getCurrentPosition(function(position) {
         self.centerLocation = [position.coords.latitude,
                                 position.coords.longitude];
         findings = self.searchByProximityTo(
             position.coords.latitude,
             position.coords.longitude
-        ).slice((page-1) * this.perPage, page * this.perPage);
+        ).slice((page - 1) * self.perPage, page * self.perPage);
+
         self.trigger('after:search', findings);
-      }, function(){
+      }, function() {
         // TODO: Find a better error message
-        self.trigger('error:search', "Geolocation is not working.");
+        self.trigger('error:search', 'Geolocation is not working.');
       });
     }
   },
 
-  searchByZipcode: function(cad, page){ // 76244
+  searchByZipcode: function(cad, page) { // 76244
     var coords = this.zipcodes[cad]; // {zipcode: [lat, lng]}
 
     if(coords){
@@ -73,14 +75,15 @@ app.Controllers.HomeController = Marionette.Controller.extend({
               slice((page-1) * this.perPage, page * this.perPage);
     } else {
       // TODO: Find a better error message
-      this.trigger('error:search', "That's not a Texas' zipcde");
+      this.trigger('error:search', 'That\'s not a Texas\' zipcde');
     }
   },
 
-  searchByName: function(name){
-    var results,
-        regex = new RegExp(name, "i");
-    results = this.hospitals.filter(function(hospital){
+  searchByName: function(name) {
+    var regex = new RegExp(name, 'i'),
+        results;
+
+    results = this.hospitals.filter(function(hospital) {
       if(hospital.get('name').toLowerCase().search(regex) !== -1){
         return hospital;
       }
@@ -89,9 +92,9 @@ app.Controllers.HomeController = Marionette.Controller.extend({
     return results;
   },
 
-  showResults: function(results){
+  showResults: function(results) {
     var collection = new app.Collections.Hospitals(results);
-    app.hospitalsView = new app.Views.Hospitals({
+    app.hospitalsView = new app.Views.Hospitals( {
       collection: collection
     });
     app.mapView = new app.Views.Map(this.centerLocation);
@@ -100,16 +103,12 @@ app.Controllers.HomeController = Marionette.Controller.extend({
     app.mapView.scrollToMap();
   },
 
-  hideResults: function(){
-    if (app.hospitalsView){
-      app.hospitalsView.close();
-    }
-    if (app.mapView){
-      app.mapView.close();
-    }
+  hideResults: function() {
+    if (app.hospitalsView) app.hospitalsView.close();
+    if (app.mapView) app.mapView.close();
   },
 
-  searchByProximityTo: function(lat1, lng1){
+  searchByProximityTo: function(lat1, lng1) {
     var output,
         self = this;
     output = this.hospitals.sortBy(function(hospital){
@@ -122,26 +121,36 @@ app.Controllers.HomeController = Marionette.Controller.extend({
     return output;
   },
 
-  distanceFromLatLng: function(lat1, lon1, lat2, lon2){
-    function deg2rad(deg) {
+  distanceFromLatLng: function(lat1, lon1, lat2, lon2) {
+    var R = 3958, // Radius of the earth in miles
+        deg2rad,
+        dLat,
+        dLon,
+        a,
+        c,
+        d;
+
+    deg2rad = function deg2rad(deg) {
       return deg * (Math.PI/180);
     }
-    var R = 3958; // Radius of the earth in miles
-    var dLat = deg2rad(lat2-lat1);
-    var dLon = deg2rad(lon2-lon1);
-    var a =
+
+    dLat = deg2rad(lat2-lat1);
+    dLon = deg2rad(lon2-lon1);
+    a =
       Math.sin(dLat/2) * Math.sin(dLat/2) +
       Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
       Math.sin(dLon/2) * Math.sin(dLon/2)
       ;
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-    var d = R * c; // Distance in miles
+    c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    d = R * c; // Distance in miles
     return d;
   },
 
-  fetchZipcodes: function(){
-    var self = this;
-    var jqxhr = $.getJSON( "/api/zipcodes.json", function(data) {
+  fetchZipcodes: function() {
+    var self = this,
+        jqxhr;
+
+    jqxhr = $.getJSON( '/api/zipcodes.json', function(data) {
       self.zipcodes = data;
     })
     .fail(function() {
