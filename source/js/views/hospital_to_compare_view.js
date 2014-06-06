833a919f3a6016a6836fd19d503f69d72aa134bc
malev <marcosvanetta@gmail.com>
2014-06-06 18:56:42 -0500

    Capture remove button click

--- source/js/views/hospital_to_compare_view.js
+++ source/js/views/hospital_to_compare_view.js
@@ -2,5 +2,18 @@
 
 app.Views.HospitalToCompare = Marionette.ItemView.extend({
   template: JST['templates/hospital-to-compare'],
-  tagName: 'tr'
+  tagName: 'tr',
+
+  ui: {
+    'removeButton': 'button.remove'
+  },
+
+  events: {
+    'click @ui.removeButton' : 'removeHospital'
+  },
+
+  removeHospital: function(event){
+    event.preventDefault();
+    this.trigger('remove:hospital', event.currentTarget.dataset.id);
+  }
 });
