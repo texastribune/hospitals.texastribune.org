0567617335bac466140815ad210ccdbef2e7d5c7
malev <marcosvanetta@gmail.com>
2014-06-09 17:43:50 -0500

    Load hospitals to compare

--- source/js/controllers/compare_controller.js
+++ source/js/controllers/compare_controller.js
@@ -2,5 +2,28 @@
 
 app.Controllers.CompareController = Marionette.Controller.extend({
   initialize: function(hospitalIds) {
+    var loadingFn = _.after(hospitalIds.length, this.loaded)
+    this.hospitalsJSON = [];
+    this.on('hospital:loading', loadingFn);
+    this.getHospitals(hospitalIds);
+  },
+
+  loaded: function() {
+    this.hospitals = new app.Collections.Hospitals(this.hospitalsJSON)
+    this.compareView = new app.Views.CompareView({
+      collection: this.hospitals
+    });
+  },
+
+  getHospitals: function(hospitalIds) {
+    var self = this,
+        jqxhr;
+
+    _.each(hospitalIds, function(hospitalId) {
+      jqxhr = $.get('/api/' + hospitalId + '.json', function(data) {
+        self.hospitalsJSON.push(data);
+        self.trigger('hospital:loading');
+      });
+    });
   }
 });
