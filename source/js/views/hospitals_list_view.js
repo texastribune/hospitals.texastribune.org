3cd50c8523eb61a9f5a79539be53ca7ef9379a55
Annie Daniel <adaniel@texastribune.org>
2015-01-19 16:52:07 -0600

    tablesorter works yay, no css though

--- source/js/views/hospitals_list_view.js
+++ source/js/views/hospitals_list_view.js
@@ -2,4 +2,8 @@ app.Views.HospitalsList = Marionette.CompositeView.extend({
   template: JST['templates/hospitals-list'],
   itemView: app.Views.HospitalListed,
   itemViewContainer: 'tbody',
+
+  onShow: function() {
+    $('table').tablesorter();
+  }
 });
