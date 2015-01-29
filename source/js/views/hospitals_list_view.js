943e2460683d45ef48a41349933c84a67310da86
Annie Daniel <adaniel@texastribune.org>
2015-01-29 13:18:56 -0600

    added print stylesheet so print doesn't print all the things

--- source/js/views/hospitals_list_view.js
+++ source/js/views/hospitals_list_view.js
@@ -7,6 +7,7 @@ app.Views.HospitalsList = Marionette.CompositeView.extend({
     var $hospitalList = $("#hospital-list-all");
     var $toggle = $('.toggle');
     var $alpha = $('.alpha');
+    var $emailHospital = $('#email-hospital');
 
     $hospitalList.tablesorter({
       sortList: [[1, 0]],
@@ -21,7 +22,13 @@ app.Views.HospitalsList = Marionette.CompositeView.extend({
       }
     });
 
-    // whyyyyyy won't you work
+    // $emailHospital.on('load', function(){
+    //   $emailHospital
+    //     .html('<a title="email" href="mailto:?subject=Texas Tribune Hospital Explorer&body=[sub] Check out the Texas Hospitals Explorer and learn more about quality of care at Texas' 377 hospitals" onclick="this.href = this.href.replace('[sub]', window.location)">')
+    // });
+
+
+
     // $toggle.on('click', function(){
     //   if ($toggle.hasClass('alpha-hide')){
     //     $toggle.removeClass('alpha-hide').addClass('alpha-show');
